22a85660c507eb82ee7f958fc99d73dea06bd33d
LuizCocada <ludehenriq@gmail.com>
2023-12-10 21:21:08 -0300

    bug sound 2.0

--- src/focusTimer/sounds.js
+++ src/focusTimer/sounds.js
@@ -3,10 +3,10 @@ export const buttonPressAudio = new Audio('Assets/button-press.wav')
 export const kichenTimer = new Audio('Assets/kichen-timer.mp3')
 
 
-export const florestSound = new Audio('assets/Floresta.wav')
-export const rainSound = new Audio('assets/Chuva.wav')
-export const coffeSound = new Audio('assets/Cafeteria.wav')
-export const fireSound = new Audio('assets/Lareira.wav')
+export const florestSound = new Audio('Assets/Floresta.wav')
+export const rainSound = new Audio('Assets/Chuva.wav')
+export const coffeSound = new Audio('Assets/Cafeteria.wav')
+export const fireSound = new Audio('Assets/Lareira.wav')
 
 florestSound.loop = true
 rainSound.loop = true
