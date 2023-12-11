7444df43d5c8985ef44e33568caf9cb2096a22bd
LuizCocada <ludehenriq@gmail.com>
2023-12-10 21:19:03 -0300

    bug sounds resolution

--- src/focusTimer/sounds.js
+++ src/focusTimer/sounds.js
@@ -1,13 +1,12 @@
 
+export const buttonPressAudio = new Audio('Assets/button-press.wav')
+export const kichenTimer = new Audio('Assets/kichen-timer.mp3')
 
-export const buttonPressAudio = new Audio('./Assets/button-press.wav')
-export const kichenTimer = new Audio('./Assets/kichen-timer.mp3')
 
-
-export const florestSound = new Audio('./assets/Floresta.wav')
-export const rainSound = new Audio('./assets/Chuva.wav')
-export const coffeSound = new Audio('./assets/Cafeteria.wav')
-export const fireSound = new Audio('./assets/Lareira.wav')
+export const florestSound = new Audio('assets/Floresta.wav')
+export const rainSound = new Audio('assets/Chuva.wav')
+export const coffeSound = new Audio('assets/Cafeteria.wav')
+export const fireSound = new Audio('assets/Lareira.wav')
 
 florestSound.loop = true
 rainSound.loop = true
